a603909a15425d420aaaa0cfc9ad0b0b625de076
rogerdpack <rogerpack2005@gmail.com>
2017-10-16 21:52:32 -0700

    update gradle/API

--- app/src/main/java/name/cpr/ExampleActivity.java
+++ app/src/main/java/name/cpr/ExampleActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import cpr.name.videoenabledwebview.R;
 
